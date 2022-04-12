f076e1e7458500fd563d369c4ae7babc0e6fd459
Darko Miletic <darko.miletic@gmail.com>
2022-04-12 09:38:49 -0300

    Security update of dompdf library. See CVE-2022-28368

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -32,7 +32,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => 'ee9cb4346511cb40aeae3e5813d899df80262995',
+    'reference' => '3c40e7279e74d55f4ea9fb8d136ee658801fc68b',
     'name' => '__root__',
   ),
   'versions' => 
@@ -44,43 +44,43 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => 'ee9cb4346511cb40aeae3e5813d899df80262995',
+      'reference' => '3c40e7279e74d55f4ea9fb8d136ee658801fc68b',
     ),
     'dompdf/dompdf' => 
     array (
-      'pretty_version' => 'v1.0.2',
-      'version' => '1.0.2.0',
+      'pretty_version' => 'v1.2.1',
+      'version' => '1.2.1.0',
       'aliases' => 
       array (
       ),
-      'reference' => '8768448244967a46d6e67b891d30878e0e15d25c',
+      'reference' => 'c6dfd9bb8b0040609f04754f729d4cb3016e0575',
     ),
     'phenx/php-font-lib' => 
     array (
-      'pretty_version' => '0.5.2',
-      'version' => '0.5.2.0',
+      'pretty_version' => '0.5.4',
+      'version' => '0.5.4.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'ca6ad461f032145fff5971b5985e5af9e7fa88d8',
+      'reference' => 'dd448ad1ce34c63d09baccd05415e361300c35b4',
     ),
     'phenx/php-svg-lib' => 
     array (
-      'pretty_version' => 'v0.3.3',
-      'version' => '0.3.3.0',
+      'pretty_version' => '0.4.1',
+      'version' => '0.4.1.0',
       'aliases' => 
       array (
       ),
-      'reference' => '5fa61b65e612ce1ae15f69b3d223cb14ecc60e32',
+      'reference' => '4498b5df7b08e8469f0f8279651ea5de9626ed02',
     ),
     'sabberworm/php-css-parser' => 
     array (
-      'pretty_version' => '8.3.1',
-      'version' => '8.3.1.0',
+      'pretty_version' => '8.4.0',
+      'version' => '8.4.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'd217848e1396ef962fb1997cf3e2421acba7f796',
+      'reference' => 'e41d2140031d533348b2192a83f02d8dd8a71d30',
     ),
   ),
 );
